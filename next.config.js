ecbebd325af465271afec8fa2f249aa324a83631
AustynWu <austynwu@gmail.com>
2025-09-17 16:38:59 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true }, // 若有用 next/image
-  // 若你的頁面會放在子目錄（例如 username.github.io/repo-name），加：
-  // basePath: '/<repo-name>',
-  // assetPrefix: '/<repo-name>/',
-};
-module.exports = nextConfig;
+const nextConfig = {}
+
+module.exports = nextConfig
